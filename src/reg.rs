c1446e6a8c59fec04f7ffe67d84fed408a1804f2
clia <clia@163.com>
2021-04-12 15:41:41 +0800

    initial commit

---
+++ src/reg.rs
@@ -0,0 +1,17 @@
+use postgres_types::{FromSql, Type};
+use postgres_types::accepts;
+use postgres_protocol::types::{self};
+use std::error::Error;
+use std::fmt::Debug;
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct RegType(pub String);
+
+impl<'a> FromSql<'a> for RegType
+{
+    fn from_sql(_ty: &Type, raw: &'a [u8]) -> Result<RegType, Box<dyn Error + Sync + Send>> {
+        types::text_from_sql(raw).map(|s| RegType(s.to_string()))
+    }
+
+    accepts!(REGTYPE);
+}
