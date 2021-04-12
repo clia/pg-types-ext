c1d64b052489e19ee630cf3c50c2b21d1eabc690
clia <clia@163.com>
2021-04-12 17:07:15 +0800

    change to impl tokio-postgres FromSql

--- src/reg.rs
+++ src/reg.rs
@@ -1,5 +1,6 @@
-use postgres_types::{FromSql, Type};
-use postgres_types::accepts;
+use postgres_types::{Type};
+// use postgres_types::accepts;
+use tokio_postgres::types::accepts;
 use postgres_protocol::types::{self};
 use std::error::Error;
 use std::fmt::Debug;
@@ -7,12 +8,24 @@ use std::fmt::Debug;
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct RegType(pub String);
 
-impl<'a> FromSql<'a> for RegType
+// impl<'a> postgres_types::FromSql<'a> for RegType
+// {
+//     fn from_sql(_ty: &Type, raw: &[u8]) -> Result<RegType, Box<dyn Error + Sync + Send>> {
+//         // for c in raw {
+//         //     print!("{}", String::from(*c as char));
+//         // }
+//         types::text_from_sql(raw).map(|s| RegType(s.to_string()))
+//     }
+
+//     accepts!(REGTYPE);
+// }
+
+impl<'a> tokio_postgres::types::FromSql<'a> for RegType
 {
     fn from_sql(_ty: &Type, raw: &[u8]) -> Result<RegType, Box<dyn Error + Sync + Send>> {
-        // for c in raw {
-        //     print!("{}", String::from(*c as char));
-        // }
+        for c in raw {
+            print!("{}", String::from(*c as char));
+        }
         types::text_from_sql(raw).map(|s| RegType(s.to_string()))
     }
 
